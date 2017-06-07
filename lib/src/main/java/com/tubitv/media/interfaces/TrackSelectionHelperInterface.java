16f50ee9e10223d211f0086d1083828d081bd271
StoyanD <s.dimitrov19@gmail.com>
2017-06-06 18:40:48 -0700

    -adding new jcenter distro plugin -pushing 0.0.2-alpha to jcenter

--- lib/src/main/java/com/tubitv/media/interfaces/TrackSelectionHelperInterface.java
+++ lib/src/main/java/com/tubitv/media/interfaces/TrackSelectionHelperInterface.java
@@ -1,14 +1,12 @@
 package com.tubitv.media.interfaces;
 
-import com.google.android.exoplayer2.trackselection.MappingTrackSelector;
-
 /**
  * Created by stoyan on 6/2/17.
  */
 public interface TrackSelectionHelperInterface {
 
     /**
-     * Callback method to inform the caller of {@link com.tubitv.media.helpers.TrackSelectionHelper#showSelectionDialog(MappingTrackSelector.MappedTrackInfo, int, TrackSelectionHelperInterface)}
+     * Callback method to inform the caller of {@link com.tubitv.media.helpers.TrackSelectionHelper#showSelectionDialog(int, TrackSelectionHelperInterface)}
      * if the track selector was overridden
      *
      * @param trackSelected True if the track selector was overridden, false if not (ie. its on auto select)
