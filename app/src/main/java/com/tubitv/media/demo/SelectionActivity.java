26618da5e6c77db82b0207ae4f6f63a4a144cdcf
StoyanD <s.dimitrov19@gmail.com>
2017-06-07 11:09:03 -0700

    -added more jcenter params -support for no subtitles

--- app/src/main/java/com/tubitv/media/demo/SelectionActivity.java
+++ app/src/main/java/com/tubitv/media/demo/SelectionActivity.java
@@ -23,7 +23,7 @@ public class SelectionActivity extends Activity{
         playHls1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url = "http://c13.adrise.tv/v2/sources/content-owners/paramount/312926/v201604161517-1024x436-,434,981,1533,2097,k.mp4.m3u8?Ku-zvPPeC4amIvKktZuE4IU69WFe1z2sTp84yvomcFQOsMka6d0EyZy1tHl3VT6-";
+                String url = "http://c13.adrise.tv/v2/sources/content-owners/paramount/312926/v201604161517-1024x436-,434,981,1533,2097,k.mp4.m3u8?-lLxsz03Q3-AGXyxib5oYdBWGCuA-XDAerU9rCntATnUsgRfb89ZeQjA5X5-x_MO";
                 String subs = "http://s.adrise.tv/94335ae6-c5d3-414d-8ff2-177c955441c6.srt";
                 String artwork = "http://images.adrise.tv/J3cemMAwJC8aX_Eb4MzIaAcNJvY=/768x362/smart/img.adrise.tv/0c690475-ea32-4b91-b704-7d3fab94a48f.jpg";
                 String name = "Gladiator";
@@ -38,7 +38,8 @@ public class SelectionActivity extends Activity{
             @Override
             public void onClick(View v) {
                 String url = "http://c13.adrise.tv/v2/sources/content-owners/cinedigm-tubi/339391/v201701300137-,210,415,677,1134,1552,k.mp4.m3u8?_GaMigbo9VgGAAHGls7iMuhpBa-WrdeZMTk2cIeb3zWjSKWPFc00aRgL98GNQ0hq";
-                String subs = "http://s.adrise.tv/55f553f9-52f1-4020-8664-fac91e7997ca.srt";
+//                String subs = "http://s.adrise.tv/55f553f9-52f1-4020-8664-fac91e7997ca.srt";
+                String subs = null;
                 String artwork = "http://images.adrise.tv/7V6_jCHzkl0lQARtDgWCnOKUozs=/492x118:1909x905/768x362/smart/img.adrise.tv/1541c7d3-9a50-4c5c-9bf7-ba63e4018f36.jpg";
                 String name = "Wild Bill";
                 Intent intent = new Intent(SelectionActivity.this, DemoActivity.class);
--- lib/src/main/java/com/tubitv/media/bindings/TubiObservable.java
+++ lib/src/main/java/com/tubitv/media/bindings/TubiObservable.java
@@ -94,6 +94,11 @@ public class TubiObservable extends BaseObservable implements ExoPlayer.EventLis
     private boolean subtitlesEnabled;
 
     /**
+     * The state of the media source tracks, if there are no subtitle tracks, hide the view
+     */
+    private boolean subtitlesExist;
+
+    /**
      * The state of the quality settings, true if user has defined a quality;
      * false if the quality is on auto
      */
@@ -464,7 +469,13 @@ public class TubiObservable extends BaseObservable implements ExoPlayer.EventLis
         notifyPropertyChanged(BR.secondaryProgressBarValue);
     }
 
-    public void setPlaybackInterface(TubiPlaybackControlInterface playbackInterface) {
+    @Bindable
+    public boolean isSubtitlesExist() {
+        return subtitlesExist;
+    }
 
+    public void setSubtitlesExist(boolean subtitlesExist) {
+        this.subtitlesExist = subtitlesExist;
+        notifyPropertyChanged(BR.subtitlesExist);
     }
 }
