b240f48a9cc01b621d3bf5368d45aeae543fe76b
Allen Sun <allengotstuff@gmail.com>
2017-08-15 17:24:39 -0700

    stateFactory enable custom state type creation

--- lib/src/main/java/com/tubitv/media/fsm/concrete/factory/StateFactory.java
+++ lib/src/main/java/com/tubitv/media/fsm/concrete/factory/StateFactory.java
@@ -4,6 +4,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.tubitv.media.fsm.State;
+import com.tubitv.media.fsm.concrete.AdPlayingState;
+import com.tubitv.media.fsm.concrete.FinishState;
+import com.tubitv.media.fsm.concrete.MakingAdCallState;
+import com.tubitv.media.fsm.concrete.MoviePlayingState;
+import com.tubitv.media.fsm.concrete.ReceiveAdState;
+import com.tubitv.media.fsm.concrete.VastAdInteractionSandBoxState;
+import com.tubitv.media.fsm.concrete.VpaidState;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -14,10 +21,31 @@ import java.util.Map;
  * Created by allensun on 7/31/17.
  * To reuse state instance, we have a caching mechanism to only create one instance of each {@link State},
  * reuse that instance from cached map.
+ * <p>
+ * The default instance of {@link com.tubitv.media.fsm.BaseState} should be created using the below class.
+ * <p>
+ * {@link com.tubitv.media.fsm.concrete.MakingAdCallState},
+ * <p>
+ * {@link com.tubitv.media.fsm.concrete.MoviePlayingState}
+ * <p>
+ * {@link com.tubitv.media.fsm.concrete.FinishState}
+ * <p>
+ * {@link com.tubitv.media.fsm.concrete.ReceiveAdState}
+ * <p>
+ * {@link com.tubitv.media.fsm.concrete.AdPlayingState}
+ * <p>
+ * {@link com.tubitv.media.fsm.concrete.VpaidState}
+ * <p>
+ * {@link com.tubitv.media.fsm.concrete.VastAdInteractionSandBoxState}
  */
 public class StateFactory {
 
-    private static final Map<Class, State> stateInstance = new HashMap<>();
+    private final Map<Class, State> stateInstance = new HashMap<>();
+
+    /**
+     * the key is the default state, and value is the custom state.
+     */
+    private final Map<Class, Class> customStateType = new HashMap<>();
 
     @Nullable
     private synchronized State getCacheInstance(@NonNull Class type) {
@@ -28,18 +56,66 @@ public class StateFactory {
         stateInstance.put(type, instance);
     }
 
+    /**
+     * @param subClass must be the subclass of {@link com.tubitv.media.fsm.BaseState} to swap original to subclass
+     */
+    public void overrideStateCreation(@NonNull Class subClass) {
+
+        if (MakingAdCallState.class.isAssignableFrom(subClass)) {
+            customStateType.put(MakingAdCallState.class, subClass);
+
+        } else if (MoviePlayingState.class.isAssignableFrom(subClass)) {
+            customStateType.put(MoviePlayingState.class, subClass);
+
+        } else if (FinishState.class.isAssignableFrom(subClass)) {
+            customStateType.put(FinishState.class, subClass);
+
+        } else if (ReceiveAdState.class.isAssignableFrom(subClass)) {
+            customStateType.put(ReceiveAdState.class, subClass);
+
+        } else if (AdPlayingState.class.isAssignableFrom(subClass)) {
+            customStateType.put(AdPlayingState.class, subClass);
+
+        } else if (VpaidState.class.isAssignableFrom(subClass)) {
+            customStateType.put(VpaidState.class, subClass);
+
+        } else if (VastAdInteractionSandBoxState.class.isAssignableFrom(subClass)) {
+            customStateType.put(VastAdInteractionSandBoxState.class, subClass);
+
+        } else {
+            throw new IllegalStateException(String.valueOf(subClass.getName() + "is not a base class of default State class "));
+        }
+    }
+
+    /**
+     * convert the default {@link State} into custom State
+     *
+     * @param cla default state type
+     * @return the custom state type
+     */
+    @Nullable
+    private Class convertToCustomClass(@NonNull Class cla) {
+        return customStateType.get(cla);
+    }
 
     public State createState(@NonNull Class classType) {
 
-        State buildState = getCacheInstance(classType);
+        // null check if there is any custom state class. if there is,
+        Class finalClassType = convertToCustomClass(classType);
+
+        if (finalClassType == null) {
+            finalClassType = classType;
+        }
+
+        State buildState = getCacheInstance(finalClassType);
 
         if (buildState == null) {
             try {
 
-                Constructor<?> ctor = classType.getConstructor();
+                Constructor<?> ctor = finalClassType.getConstructor();
                 buildState = (State) ctor.newInstance();
 
-                setCacheInstance(classType, buildState);
+                setCacheInstance(finalClassType, buildState);
             } catch (NoSuchMethodException e) {
                 e.printStackTrace();
             } catch (IllegalAccessException e) {
---
+++ lib/src/test/java/com/tubitv/demo/ExoPlayerFactoryTest.java
@@ -0,0 +1,246 @@
+package com.tubitv.demo;
+
+import com.tubitv.media.fsm.Input;
+import com.tubitv.media.fsm.State;
+import com.tubitv.media.fsm.concrete.AdPlayingState;
+import com.tubitv.media.fsm.concrete.FinishState;
+import com.tubitv.media.fsm.concrete.MakingAdCallState;
+import com.tubitv.media.fsm.concrete.MoviePlayingState;
+import com.tubitv.media.fsm.concrete.ReceiveAdState;
+import com.tubitv.media.fsm.concrete.VastAdInteractionSandBoxState;
+import com.tubitv.media.fsm.concrete.VpaidState;
+import com.tubitv.media.fsm.concrete.factory.StateFactory;
+import com.tubitv.media.fsm.state_machine.FsmPlayer;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by allensun on 8/15/17.
+ * on Tubitv.com, allengotstuff@gmail.com
+ */
+
+@RunWith(JUnit4.class)
+public class ExoPlayerFactoryTest {
+
+    StateFactory stateFactory;
+
+    FsmPlayer playerFsm;
+
+
+    @Before
+    public void setup() {
+
+        stateFactory = new StateFactory();
+        stateFactory.overrideStateCreation(TestMoviePlayingState.class);
+        stateFactory.overrideStateCreation(TestMakingAdCallState.class);
+        stateFactory.overrideStateCreation(TestFinishState.class);
+        stateFactory.overrideStateCreation(TestReceivedState.class);
+        stateFactory.overrideStateCreation(TestAdPlayingState.class);
+        stateFactory.overrideStateCreation(TestVastAdSandBox.class);
+        stateFactory.overrideStateCreation(TestVpadState.class);
+
+        playerFsm = new FsmPlayer(stateFactory) {
+            @Override
+            public Class initializeState() {
+                return MoviePlayingState.class;
+            }
+        };
+    }
+
+
+    @Test
+    public void testCustomClass() {
+
+        State state = stateFactory.createState(MoviePlayingState.class);
+
+        assertThat(state instanceof TestMoviePlayingState, is(true));
+
+        // make ad call
+
+        state = stateFactory.createState(MakingAdCallState.class);
+
+        assertThat(state instanceof TestMakingAdCallState, is(true));
+
+        //finish state
+
+        state = stateFactory.createState(FinishState.class);
+
+        assertThat(state instanceof TestFinishState, is(true));
+
+        //receive ad state
+
+        state = stateFactory.createState(ReceiveAdState.class);
+
+        assertThat(state instanceof TestReceivedState, is(true));
+
+        //ad playing state
+
+        state = stateFactory.createState(AdPlayingState.class);
+
+        assertThat(state instanceof TestAdPlayingState, is(true));
+
+        //vast sandbox
+
+        state = stateFactory.createState(VastAdInteractionSandBoxState.class);
+
+        assertThat(state instanceof TestVastAdSandBox, is(true));
+
+        //vpaid
+
+        state = stateFactory.createState(VpaidState.class);
+
+        assertThat(state instanceof TestVpadState, is(true));
+
+
+        state = stateFactory.createState(MoviePlayingState.class);
+
+        assertThat(state instanceof TestMoviePlayingState, is(true));
+
+    }
+
+    @Test
+    public void testFSMFlowWithVpaid() {
+
+        playerFsm.transit(Input.INITIALIZE);
+
+        playerFsm.transit(Input.MAKE_AD_CALL);
+
+        assertTrue(playerFsm.getCurrentState() instanceof MakingAdCallState);
+
+        playerFsm.transit(Input.AD_RECEIVED);
+
+        assertTrue(playerFsm.getCurrentState() instanceof ReceiveAdState);
+
+        playerFsm.transit(Input.SHOW_ADS);
+
+        assertTrue(playerFsm.getCurrentState() instanceof AdPlayingState);
+
+        playerFsm.transit(Input.NEXT_AD);
+
+        assertTrue(playerFsm.getCurrentState() instanceof AdPlayingState);
+
+        playerFsm.transit(Input.AD_CLICK);
+
+        assertTrue(playerFsm.getCurrentState() instanceof VastAdInteractionSandBoxState);
+
+        playerFsm.transit(Input.BACK_TO_PLAYER_FROM_VAST_AD);
+
+        assertTrue(playerFsm.getCurrentState() instanceof AdPlayingState);
+
+        playerFsm.transit(Input.VPAID_MANIFEST);
+
+        assertTrue(playerFsm.getCurrentState() instanceof VpaidState);
+
+        playerFsm.transit(Input.BACK_TO_PLAYER_FROM_VPAID_AD);
+
+        assertTrue(playerFsm.getCurrentState() instanceof MoviePlayingState);
+
+        playerFsm.transit(Input.MAKE_AD_CALL);
+
+        assertTrue(playerFsm.getCurrentState() instanceof MakingAdCallState);
+
+        playerFsm.transit(Input.EMPTY_AD);
+
+        assertTrue(playerFsm.getCurrentState() instanceof MoviePlayingState);
+
+        playerFsm.transit(Input.MOVIE_FINISH);
+
+        assertTrue(playerFsm.getCurrentState() instanceof FinishState);
+    }
+
+    @Test
+    public void testFSMFlowWithNoVpaid() {
+
+        playerFsm.transit(Input.INITIALIZE);
+
+        for (int i = 0; i < 10; i++) {
+
+            playerFsm.transit(Input.MAKE_AD_CALL);
+
+            assertTrue(playerFsm.getCurrentState() instanceof MakingAdCallState);
+
+            playerFsm.transit(Input.AD_RECEIVED);
+
+            assertTrue(playerFsm.getCurrentState() instanceof ReceiveAdState);
+
+            playerFsm.transit(Input.SHOW_ADS);
+
+            assertTrue(playerFsm.getCurrentState() instanceof AdPlayingState);
+
+            playerFsm.transit(Input.NEXT_AD);
+
+            assertTrue(playerFsm.getCurrentState() instanceof AdPlayingState);
+
+            playerFsm.transit(Input.AD_CLICK);
+
+            assertTrue(playerFsm.getCurrentState() instanceof VastAdInteractionSandBoxState);
+
+            playerFsm.transit(Input.BACK_TO_PLAYER_FROM_VAST_AD);
+
+            assertTrue(playerFsm.getCurrentState() instanceof AdPlayingState);
+
+            playerFsm.transit(Input.AD_CLICK);
+
+            assertTrue(playerFsm.getCurrentState() instanceof VastAdInteractionSandBoxState);
+
+            playerFsm.transit(Input.BACK_TO_PLAYER_FROM_VAST_AD);
+
+            assertTrue(playerFsm.getCurrentState() instanceof AdPlayingState);
+
+            playerFsm.transit(Input.NEXT_AD);
+
+            assertTrue(playerFsm.getCurrentState() instanceof AdPlayingState);
+
+            playerFsm.transit(Input.VPAID_MANIFEST);
+
+            assertTrue(playerFsm.getCurrentState() instanceof VpaidState);
+
+            playerFsm.transit(Input.BACK_TO_PLAYER_FROM_VPAID_AD);
+
+            assertTrue(playerFsm.getCurrentState() instanceof MoviePlayingState);
+
+            playerFsm.transit(Input.MAKE_AD_CALL);
+
+        }
+
+        playerFsm.transit(Input.EMPTY_AD);
+
+        playerFsm.transit(Input.MOVIE_FINISH);
+
+        assertTrue(playerFsm.getCurrentState() instanceof FinishState);
+    }
+
+    @Test
+    public void testErrorFlow() {
+        testFSMFlowWithNoVpaid();
+
+        playerFsm.transit(Input.AD_CLICK);
+        playerFsm.transit(Input.BACK_TO_PLAYER_FROM_VAST_AD);
+        playerFsm.transit(Input.AD_FINISH);
+        playerFsm.transit(Input.AD_CLICK);
+
+
+        assertTrue(playerFsm.getCurrentState() instanceof MoviePlayingState);
+    }
+
+    public static class TestMoviePlayingState extends MoviePlayingState {}
+
+    public static class TestMakingAdCallState extends MakingAdCallState {}
+
+    public static class TestFinishState extends FinishState {}
+
+    public static class TestReceivedState extends ReceiveAdState {}
+
+    public static class TestAdPlayingState extends AdPlayingState {}
+
+    public static class TestVastAdSandBox extends VastAdInteractionSandBoxState {}
+
+    public static class TestVpadState extends VpaidState {}
+}
