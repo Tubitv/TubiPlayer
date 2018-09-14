2d319e26b818c3c0e1bfaa15c75a9dc9bb09b279
Heming Liu <liuhm1113@gmail.com>
2018-09-14 15:59:43 -0700

    Check fire tv device in isTV check

--- lib/src/main/java/com/tubitv/media/utilities/PlayerDeviceUtils.java
+++ lib/src/main/java/com/tubitv/media/utilities/PlayerDeviceUtils.java
@@ -11,12 +11,17 @@ public class PlayerDeviceUtils {
     private static final String TAG = PlayerDeviceUtils.class.getSimpleName();
     private static final String XIAOMI_MANUFACTURER = "Xiaomi";
     private static final String MI_BOX_DEVICE = "once";
+    private static final String AMAZON_FEATURE_FIRE_TV = "amazon.hardware.fire_tv";
     private static Boolean sIsTVDevice = null;
 
     public static boolean isTVDevice(final Context context) {
         if (sIsTVDevice == null) {
             UiModeManager uiModeManager = (UiModeManager) context.getSystemService(UI_MODE_SERVICE);
             sIsTVDevice = uiModeManager.getCurrentModeType() == Configuration.UI_MODE_TYPE_TELEVISION;
+
+            if (!sIsTVDevice) { // We also check fire tv
+                sIsTVDevice = context.getPackageManager().hasSystemFeature(AMAZON_FEATURE_FIRE_TV);
+            }
         }
         return sIsTVDevice;
     }
