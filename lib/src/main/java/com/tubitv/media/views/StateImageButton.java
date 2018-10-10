64f119c5334d1928e8a83b0cbef8f77c7ee7483d
Kobee <kobee@tubi.tv>
2018-10-10 15:38:53 +0800

    remove disabled state; do some improvements

--- lib/src/main/java/com/tubitv/media/views/StateImageButton.java
+++ lib/src/main/java/com/tubitv/media/views/StateImageButton.java
@@ -49,18 +49,8 @@ public class StateImageButton extends ImageButton {
         init(attrs);
     }
 
-    @BindingAdapter("bind:enabledState")
-    public static void onEnabledStateChanged(StateImageButton imageButton, boolean enabled) {
-        System.out.println("wkwDebug onEnabledStateChanged enabled=" + enabled);
-        if (!enabled) {
-            imageButton.setChecked(false);
-        }
-        imageButton.setEnabled(enabled);
-    }
-
     @BindingAdapter("bind:checkedState")
     public static void onCheckedStateChanged(StateImageButton imageButton, boolean checked) {
-        System.out.println("wkwDebug onCheckedStateChanged checked=" + checked);
         imageButton.setChecked(checked);
     }
 
