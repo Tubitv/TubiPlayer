f93b1d97822d9490a23fe9e974a01671566f591e
dahis39 <smon_lucia@Hotmail.com>
2019-05-23 20:46:00 -0700

    Remove unused import

--- lib/src/main/java/com/tubitv/media/controller/PlayerUIController.java
+++ lib/src/main/java/com/tubitv/media/controller/PlayerUIController.java
@@ -3,8 +3,6 @@ package com.tubitv.media.controller;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.webkit.WebView;
-import android.widget.Toast;
-
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.tubitv.media.utilities.PlayerDeviceUtils;
--- lib/src/main/java/com/tubitv/media/utilities/PlaybackSettingMenu.java
+++ lib/src/main/java/com/tubitv/media/utilities/PlaybackSettingMenu.java
@@ -8,7 +8,6 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-
 import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.tubitv.media.models.PlaybackSpeed;
